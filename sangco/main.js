a2ea4f0086cfb0b496de6f2f597071d862d45025
Chulmin Chae <chulminchae@CHULMING.local>
2021-05-17 23:04:21 +0900

    Finish basic nodejs class

--- sangco/main.js
+++ sangco/main.js
@@ -2,36 +2,9 @@ var http = require('http');
 var fs = require('fs');
 var url = require('url');
 var qs = require('querystring')
-
-var template = {
-  html:function(title, list, body, control){
-    return `
-    <!doctype html>
-    <html>
-    <head>
-      <title>WEB1 - ${title}</title>
-      <meta charset="utf-8">
-    </head>
-    <body>
-      <h1><a href="/">WEB</a></h1>
-      ${list}
-      ${control}
-      ${body}
-    </body>
-    </html>
-    `;
-  },
-  list:function (filelist){
-    var list = '<ul>';
-    var i = 0;
-    while(i < filelist.length){
-      list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
-      i = i + 1;
-    }
-    list = list+'</ul>';
-    return list;
-  }
-}
+var template = require('./lib/template.js')
+var path = require('path')
+var sanitizeHtml = require('sanitize-html')
 
  var app = http.createServer(function(request, response){
      var _url = request.url;
@@ -52,15 +25,18 @@ var template = {
          }
          else {
             fs.readdir('./data', function(error, filelist){
-              fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
+              var filteredId = path.parse(queryData.id).base;
+              fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
                 var title = queryData.id;
+                var sanitizeTitle = sanitizeHtml(title);
+                var sanitizeDescription = sanitizeHtml(description);
                 var list = template.list(filelist);
                 var html = template.html(title, list, 
-                  `<h2>${title}</h2>${description}`,`
+                  `<h2>${sanitizeTitle}</h2>${sanitizeDescription}`,`
                     <a href="/create">create</a>
-                    <a href="/update?id=${title}">update</a>
+                    <a href="/update?id=${sanitizeTitle}">update</a>
                     <form action="delete_process" method="post">
-                    <input type="hidden" name="id" value="${title}">
+                    <input type="hidden" name="id" value="${sanitizeTitle}">
                     <input type="submit" value="delete"> </form>
                   `);
                 response.writeHead(200);
@@ -106,7 +82,8 @@ var template = {
     }
     else if(pathname === '/update'){
       fs.readdir('./data', function(error, filelist){
-        fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
+        var filteredId = path.parse(queryData.id).base;
+        fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
           var title = queryData.id;
           var list = template.list(filelist);
           var html = template.html(title, list, `
@@ -160,8 +137,8 @@ var template = {
       request.on('end', function(){
           var post = qs.parse(body);
           var id = post.id;
-
-          fs.unlink(`data/${id}`, function(err){
+          var filteredId = path.parse(id).base;
+          fs.unlink(`data/${filteredId}`, function(err){
             response.writeHead(302, {Location: `/`});
             response.end('success');
           });
